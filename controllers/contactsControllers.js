2af926934e6813d33089cc21451b277983efba0b
byvova <133043645+byvova@users.noreply.github.com>
2024-05-30 17:26:28 +0300

    Update hw 2

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,50 @@
+import { Contact } from "../models/contact.js";
+import HttpError from "../helpers/HttpError.js";
+
+export const getAllContacts = async (req, res) => {
+  const result = await Contact.find();
+  res.json(result);
+};
+
+export const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  console.log(req.params);
+  const result = await Contact.findById(id);
+
+  if (!result) {
+    return res.status(404).json({ message: HttpError(404).message });
+  }
+  res.json(result);
+};
+
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndDelete(id);
+  if (!result) {
+    res.status(404).json({ message: HttpError(404).message });
+  }
+  res.json(result);
+};
+
+export const createContact = async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+export const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body);
+  if (!result) {
+    res.status(404).json({ message: HttpError(404).message });
+  }
+  res.json(result);
+};
+
+export const updateStatusContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body);
+  if (!result) {
+    res.status(404).json({ message: HttpError(404).message });
+  }
+  res.json(result);
+};
---
+++ routes/contactsRouter.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
+  updateStatusContact,
+} from "../controllers/contactsControllers.js";
+import validateBody from "../helpers/validateBody.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+  updateFavoriteSchema,
+} from "../schemas/contactsSchemas.js";
+
+const contactsRouter = express.Router();
+
+contactsRouter.get("/", getAllContacts);
+
+contactsRouter.get("/:id", getOneContact);
+
+contactsRouter.delete("/:id", deleteContact);
+
+contactsRouter.post("/", validateBody(createContactSchema), createContact);
+
+contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
+contactsRouter.patch(
+  "/:id/favorite",
+  validateBody(updateFavoriteSchema),
+  updateStatusContact
+);
+
+export default contactsRouter;
