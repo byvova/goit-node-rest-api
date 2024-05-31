5e6d61616fb95cd7a229821875be478783648437
byvova <133043645+byvova@users.noreply.github.com>
2024-05-31 16:55:44 +0300

    delete main

--- helpers/handleMongooseError.js
+++
@@ -1,6 +0,0 @@
-export const handleMongooseError = (error, data, next) => {
-  const { name, code } = error;
-  const status = name === "MongoServerError" && code === 110 ? 409 : 400;
-  error.status = status;
-  next();
-};
